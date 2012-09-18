1ba8d2ea29f83ec94b71afd3cc56665a730937ba
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2012-09-18 16:36:30 -0500

    updated script to work with multisite, better capistrano compat

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-set :stages, %w(production playground)
-set :default_stage, "playground"
+set :stages, %w(production staging)
+set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
 set :application, "application-name"
-set :repository, "git@github.com:username/repository-name.git"
+set :repository, "git@github.com:/username/repo.git"
 set :scm, :git
 set :use_sudo, false
 
@@ -11,18 +11,18 @@ ssh_options[:forward_agent] = true
 set :deploy_via, :remote_cache
 set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 set :git_enable_submodules, 1
-set :wp_multisite, 0 #set to 1 if site is multisite
+set :wp_multisite, 1
 
 namespace :myproject do
     task :symlink, :roles => :app do
         run "if [ -d #{shared_path}/uploads ]; then ln -nfs #{shared_path}/uploads #{release_path}/wp-content/uploads; fi"
         if :wp_multisite == 1
-        	run "if [ -d #{shared_path}/uploads ]; then ln -nfs #{shared_path}/blogs.dir #{release_path}/wp-content/blogs.dir; fi"
+        	run "if [ -d #{shared_path}/blogs.dir ]; then ln -nfs #{shared_path}/blogs.dir #{release_path}/wp-content/blogs.dir; fi"
         end
     end
 end
 
-after "deploy:symlink", "myproject:symlink"
+after "deploy:create_symlink", "myproject:create_symlink"
 
 ## Use below if you want to deploy based on tag releases
 #set :branch do
---
+++ config/deploy/staging.rb
@@ -0,0 +1,171 @@
+#Where we're deploying to on the server
+set :deploy_to, "/var/www/vhosts/site.com/capistrano/" # No trailing slash 
+
+#Remote Server credentials
+set :user, "user"
+
+set :domain, "domain.com"
+set :password, "password"
+set :port, "22"
+server "#{user}@#{domain}", :app #This line doesn't need to change
+
+# This is useful if apache or something needs to belong to a group. I added
+# this when working on a Med Temple DV server where the files needed to be
+# a part of the group placln. Don't forget to uncomment out the line near the
+# bottom of the document to turn this on.
+
+set :group. "group" 
+
+
+# We'll be referencing this when we go to search and replace the database, this
+# is where we'll clone the search and replace script if it doesn't exist
+set :local_path, '~/Sites/localhost' 
+
+# Local Database credentials for playground.rb
+set :local_db_database, "database"
+set :local_db_username, "user"
+set :local_db_password, "password"
+set :local_db_host, "localhost"
+set :local_db_charset, "utf8"
+set :local_site_url, "://local_sitename" #this is the url to be searched for later
+set :db_prefix, 'db_prefix_'
+
+# Remote Database credentials for playground.rb
+set :remote_db_database, "database"
+set :remote_db_username, "user"
+set :remote_db_password, "password"
+set :remote_db_host, "localhost"
+set :remote_db_charset, "utf8"
+set :remote_site_url, "://domain.com" #this is the url to be replaced with later
+
+set :dbp, "#{remote_db_database}.#{db_prefix}"
+
+
+# This is the path to WordPress's uploads folder on YOUR machine
+set :uploads_path, "/path/to/wp-content/uploads"
+
+# This is the path to WordPress's blogs.dir folder on YOUR machine (Used in MultiSite)
+set :blogs_dir_path, "/path/to/wp-content/blogs.dir"
+
+
+#Begin the magic
+
+#Symlink wp-config to the release folder so it doesn't get overwritten
+namespace :my_project do
+    task :symlink, :roles => :app do
+        run "ln -nfs #{shared_path}/wp-config-staging.php #{release_path}/wp-config-staging.php"
+        run "ln -nfs #{shared_path}/.htaccess #{release_path}/.htaccess"
+        run "ln -nfs #{shared_path}/uploads #{release_path}/wp-content/uploads"
+        run "ln -nfs #{shared_path}/blogs.dir #{release_path}/wp-content/blogs.dir"
+    end
+end
+
+after "deploy:finalize_update", "staging:symlink"
+
+#Create backups directory
+namespace(:deploy) do
+  desc "Add backups directory"
+  task :add_backups_dir, :roles => :app do
+    run "mkdir #{shared_path}/backups"
+  end
+end
+after "deploy:setup", "deploy:add_backups_dir"
+
+#Backup Remote DB
+namespace(:deploy) do
+  desc "Backup MySQL Database"
+  task :mysqlbackup, :roles => :app do
+    run "mysqldump -u#{remote_db_username} -p#{remote_db_password} #{remote_db_database} > #{shared_path}/backups/#{release_name}.sql"
+  end
+end
+before "deploy:create_symlink", "deploy:mysqlbackup"
+
+#Restore Remote DB
+namespace(:deploy) do
+  desc "Restore MySQL Database"
+  task :mysqlrestore, :roles => :app do
+    backups = capture("ls -1 #{shared_path}/backups/").split("\n")
+    default_backup = backups.last
+    puts "Available backups: "
+    puts backups
+    backup = Capistrano::CLI.ui.ask "Which backup would you like to restore? [#{default_backup}] "
+    backup_file = default_backup if backup.empty?
+
+    run "mysql -u#{remote_db_username} -p#{remote_db_password} #{remote_db_database} < #{shared_path}/backups/#{backup_file}"
+  end
+end
+
+#Dump Local DB, replace Remote DB, update urls in DB
+namespace(:deploy) do
+  desc "Dump Local DB and replace Remote DB"
+  task :dbupdate, :roles => :app do
+    run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
+    run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"  
+    run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
+    #run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_path} -r #{remote_site_path} guid=0"
+  end
+end
+
+#Dump Local DB, replace Remote DB, update urls in DB
+namespace(:deploy) do
+  desc "Dump Local DB and replace Remote DB"
+  task :mu_update, :roles => :app do
+    run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} --ignore-table=#{dbp}blogs --ignore-table=#{dbp}blog_versions --ignore_table=#{dbp}site --ignore-table=#{dbp}sitemeta | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
+    run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"  
+    run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
+  end
+end
+
+#Dump Local DB, replace Remote DB, update urls in DB
+namespace(:deploy) do
+  desc "Dump Local DB and replace Remote DB"
+  task :local_mu_update, :roles => :app do
+    run_locally "ssh #{user}@#{domain} -p #{port} 'mysqldump -u#{remote_db_username} -p#{remote_db_password} -h#{remote_db_host} #{remote_db_database} -C -c --skip-add-locks --ignore-table=#{dbp}blogs --ignore-table=#{dbp}blog_versions --ignore_table=#{dbp}site --ignore-table=#{dbp}sitemeta' | mysql -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} #{local_db_database}"
+    run "[ -d #{local_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{local_path}/srdb"  
+    run_locally "php -f #{local_path}/srdb/searchreplacedb2cli.php cli -h #{local_db_host} -d #{local_db_database} -u #{local_db_username} -p #{local_db_password} -c #{local_db_charset} -s #{remote_site_url} -r #{local_site_url} guid=0"
+  end
+end
+
+#Dump Local DB, replace Remote DB, update urls in DB
+namespace(:deploy) do
+  desc "Dump Local DB and replace Remote DB"
+  task :update_posts, :roles => :app do
+    run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} #{db_prefix}posts #{db_prefix}postmeta #{db_prefix}options | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
+    run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"  
+    run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
+  end
+end
+
+#after "deploy:mysqlbackup", "deploy:dbupdate"
+
+#Sync uploads folder
+namespace(:deploy) do
+  desc "Sync Uploads folder"
+  task :sync_uploads, :roles => :app do
+    run_locally "if [ -d #{uploads_path} ]; then rsync -avhru #{uploads_path} -delete -e 'ssh -p #{port}' #{user}@#{domain}:#{shared_path}; fi"
+  end
+end
+
+after "tft_staging:symlink", "deploy:sync_uploads"
+
+
+#Sync blogs.dir folder
+namespace(:deploy) do
+  desc "Sync blogs.dir folder"
+  task :sync_blogs_dir, :roles => :app do
+    run_locally "if [ -d #{blogs_dir_path} ]; then rsync -avhru #{blogs_dir_path} -delete -e 'ssh -p #{port}' #{user}@#{domain}:#{shared_path}; fi"
+  end
+end
+after "tft_staging:symlink", "deploy:sync_blogs_dir"
+
+#Dump Local DB, replace Remote DB, update urls in DB
+namespace(:deploy) do
+  desc "Fix file permissions"
+  task :update_permissions, :roles => :app do
+    run "cd #{release_path} && chown -R #{user}:#{group} . && find . -type d -print0 | xargs -0 chmod 755"
+    run "cd #{shared_path}/uploads && chown -R #{user}:#{group} ."
+    run "cd #{shared_path}/blogs.dir && chown -R #{user}:#{group} ."
+  end
+end
+
+#after "tft_staging:symlink", "deploy:update_permissions"
